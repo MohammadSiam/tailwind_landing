30fcab7a399cd64f3a43064625f11d8a2bc61637
MohammadSiam <siamk417@gmail.com>
2024-01-04 01:08:43 +0600

    done with hero section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-blue': '#3490dc', // Add a custom color named 'custom-blue'
+        },
+      },
+    },
+  };
+  
